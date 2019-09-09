d2881fd39b1dbe57e8cfa2964984ff139624ae2d
Augusto Queirantes <augusto_queirantes@outlook.com>
2019-09-09 08:40:02 -0300

    Add login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="login">
-    <h1>This is an login page</h1>
+  <div class="page">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  v-model="email"
+                  label="Email"
+                  type="text"
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  label="Password"
+                  type="password"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" @click="login">Login</v-btn>
+            </v-card-actions>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
   </div>
 </template>
